1c1b1f35ad747079c26840cc08be58db36b54b70
DedierJr <dedieraguiar@gmail.com>
2024-01-29 20:55:26 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const questions = document.querySelectorAll('.question');
+
+    questions.forEach(function (question) {
+        question.addEventListener('click', function () {
+            this.classList.toggle('expanded');
+        });
+    });
+});
